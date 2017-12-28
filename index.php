e2d068f9d09b421edac85619b0ec687945b26155
Matthew Sevey <mjsevey@gmail.com>
2017-12-28 16:46:48 -0500

    hiding learn up link when signed in

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 
 //Contains connectDB.php and decides which header to display
 //Also contains $userRow and $password which are the users info from the users table and their last entry in the passes table
 include("./chooseHeader.php");
-  
+
 
 ?>
-    
+
     <div class="container">
 
       <div class="jumbotron">
@@ -21,7 +21,7 @@ include("./chooseHeader.php");
             <div class="col-md-6">
               <span class="glyphicon glyphicon-thumbs-up" style="font-size: 4em;"></span>
             </div>
-          </div> 
+          </div>
 
       </div>
 
@@ -40,7 +40,9 @@ include("./chooseHeader.php");
           <h1>Have it all!</h1>
           <br>
           <p>With the Rock Pass you can have it all!  The Rock Pass allows you to go to any rock gym!  No more choosing between bouldering and leading, friends and convience.  Climb whenever, wherever.</p>
-          <p><a class="btn btn-primary btn-lg" href="./signUp.php" data-toggle="modal" role="button">Learn more</a></p>
+          <?php if (!$session) {
+            echo '<p><a class="btn btn-primary btn-lg" href="./signUp.php" data-toggle="modal" role="button">Learn more</a></p>';
+          } ?>
 
       </div>
 
